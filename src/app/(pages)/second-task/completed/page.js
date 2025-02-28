032277084fb52562c716099a568b661b216c1754
Jaine <jaineee00@gmail.com>
2025-02-27 20:08:29 -0500

    Second tasks

---
+++ src/app/(pages)/second-task/completed/page.js
@@ -0,0 +1,52 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+import Template from '@/components/template'
+import { useRouter } from 'next/navigation'
+
+export default function NarrativePage() {
+  const [currentIndex, setCurrentIndex] = useState(0)
+  const router = useRouter()
+
+  const scenes = [
+    {
+    // Before here, there should be some effects looks like interrupted from something.
+      characterName: 'Fen',
+      paragraphText: 'Excellent work! You’ve discovered the correct translation. In turn, you now know what fate this darkness will unleash upon your world. ',
+      bgImage: '/bc-1.png',
+      characterImageSrc: '/fen/fen-thumb.png',
+    },
+    {
+      characterName: 'Fen',
+      paragraphText: 'To continue your efforts to prevent your world from experiencing the same end as mine, head to the cafeteria and locate a QR code.',
+      bgImage: '/bc-1.png',
+      characterImageSrc: '/fen/fen-d.png',
+    },
+  ]
+
+  const handleNext = () => {
+    setCurrentIndex((prev) => (prev + 1) % scenes.length)
+  }
+
+  const handleBack = () => {
+    setCurrentIndex((prev) => (prev === 0 ? scenes.length - 1 : prev - 1))
+  }
+
+  useEffect(() => {
+    if (currentIndex === 3) {
+      router.push('/second-task/card')
+    }
+  }, [currentIndex, router])
+
+  return (
+    <Template
+      characterName={scenes[currentIndex].characterName}
+      characterImageSrc={scenes[currentIndex].characterImageSrc}
+      paragraphText={scenes[currentIndex].paragraphText}
+      bgImage={scenes[currentIndex].bgImage}
+      onNext={handleNext}
+      onBack={handleBack}
+      showBackButton={currentIndex !== 0}
+    />
+  )
+}
---
+++ src/app/(pages)/second-task/pink-hallway/page.js
@@ -0,0 +1,52 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+import Template from '@/components/template'
+import { useRouter } from 'next/navigation'
+
+export default function NarrativePage() {
+  const [currentIndex, setCurrentIndex] = useState(0)
+  const router = useRouter()
+
+  const scenes = [
+    {
+    // Before here, there should be some effects looks like interrupted from something.
+      characterName: 'Fen',
+      paragraphText: 'In this hallway, there are several hidden multi-linguistic clues. But which is the right one?',
+      bgImage: '/bc-1.png',
+      characterImageSrc: '/fen/fen-e.png',
+    },
+    {
+      characterName: 'Fen',
+      paragraphText: 'With your help, this next task involves translating and determining which phrase is the direct translation of the word: “Koyaanisqatsi”.',
+      bgImage: '/bc-1.png',
+      characterImageSrc: '/fen/fen-c.png',
+    },
+  ]
+
+  const handleNext = () => {
+    setCurrentIndex((prev) => (prev + 1) % scenes.length)
+  }
+
+  const handleBack = () => {
+    setCurrentIndex((prev) => (prev === 0 ? scenes.length - 1 : prev - 1))
+  }
+
+  useEffect(() => {
+    if (currentIndex === 3) {
+      router.push('/second-task/card')
+    }
+  }, [currentIndex, router])
+
+  return (
+    <Template
+      characterName={scenes[currentIndex].characterName}
+      characterImageSrc={scenes[currentIndex].characterImageSrc}
+      paragraphText={scenes[currentIndex].paragraphText}
+      bgImage={scenes[currentIndex].bgImage}
+      onNext={handleNext}
+      onBack={handleBack}
+      showBackButton={currentIndex !== 0}
+    />
+  )
+}
